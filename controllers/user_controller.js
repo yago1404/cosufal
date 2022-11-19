e350e5565c556d6ebacb9cfce151cb49d2beddc5
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2022-11-19 00:28:29 -0300

    add check auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userRepository = require('../repositories/user_repository');
 const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("../middlewares/auth_middleware");
 
 router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
@@ -10,6 +11,11 @@ router.post('/auth', async (req, res) => {
     jwt.sign({'id': user.id}, process.env.SECRET, {expiresIn: '1800s'}, async (err, generatedToken) => {
         return res.status(200).json({'status': 200, 'result': {'accessToken': generatedToken, 'user': user}});
     });
+});
+
+router.get('/check-auth', authMiddleware, async (req, res) => {
+    let { user } = req;
+    return res.status(200).json({'status': 200, 'result': user});
 });
 
 module.exports = router;
--- repositories/user_repository.js
+++ repositories/user_repository.js
@@ -1,14 +1,37 @@
 const { query } = require("../config/db");
+
+const composeUser = async function (user) {
+    user.academic_unity = await findAcademicUnityById(user.academic_unity);
+    return user;
+}
+
+const findAcademicUnityById = async function (id) {
+    let response = await query('SELECT * FROM academic_unity WHERE id = $1', [id]);
+    if (response.rows.length === 0) return undefined;
+    let academicUnity = response.rows[0];
+    academicUnity.localization = await findLocalizationById(academicUnity.localization);
+    return academicUnity;
+}
+
+const findLocalizationById = async function (id) {
+    let response = await query('SELECT * FROM localization WHERE id = $1', [id]);
+    return response.rows[0];
+}
+
 const findUserById = async function(id) {
     let response = await query('SELECT * FROM app_user WHERE id = $1', [id] );
-    if (response.rows.length > 0) return response.rows[0];
+    if (response.rows.length === 0) return undefined;
+    let user = response.rows[0];
+    delete user.password;
+    return await composeUser(user);
 }
 
 const doLogin = async function(email, password) {
     let response = await query('SELECT * FROM app_user WHERE email = $1 AND password = $2', [email, password]);
-    delete response.rows[0].password;
-    return response.rows[0];
-
+    if (response.rows.length === 0) return undefined;
+    let user = response.rows[0];
+    delete user.password;
+    return await composeUser(user);
 }
 
 module.exports = { doLogin, findUserById };
