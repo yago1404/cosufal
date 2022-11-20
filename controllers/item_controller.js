ca77cb37c033646f1fd9d4dbe4bc3c45d45b20fc
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2022-11-20 16:49:50 -0300

    post item

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -8,4 +8,13 @@ router.get('/get-all-itens', authMiddleware, async (req, res) => {
     return res.status(200).json({'status': 200, 'result': {'itens': itens}});
 });
 
+router.post('/post-item', authMiddleware, async (req, res) => {
+    let body = req.body;
+    if (body.name === undefined || body.description === undefined || body.location === undefined || body.amount === undefined) {
+        return res.status(400).json({'status': 400, 'result': {'message': 'Bad request'}});
+    }
+    await itensRepository.addItem(body);
+    return res.status(200).json({'status': 200, 'result': {'message': 'success on save ' + body.name}});
+});
+
 module.exports = router;
--- repositories/item_repository.js
+++ repositories/item_repository.js
@@ -1,7 +1,11 @@
 const { query } = require("../config/db");
 const academicUnityRepository = require("../repositories/academic_unity_repository");
 
-const getAllItens = async function() {
+const addItem = async function (item) {
+    await query('INSERT INTO item(name, amount, status, description, location) VALUES ($1, $2, True, $3, $4)', [item.name, item.amount, item.description, item.location]);
+}
+
+const getAllItens = async function () {
     const result = await query('SELECT * FROM item');
     if (result.rows.length > 0) {
         for (let item of result.rows) {
@@ -13,4 +17,4 @@ const getAllItens = async function() {
     return result.rows;
 }
 
-module.exports = { getAllItens };
+module.exports = { getAllItens, addItem };
